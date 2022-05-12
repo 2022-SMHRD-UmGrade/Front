f36ab56784bbfa566ef791b418518e1ce7eb04b2
yeeun-b <yeeun.park.417@gmail.com>
2022-05-12 11:26:55 +0900

    커뮤니티-게시판 연결 -예은-

--- front-end/umgrade/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/umgrade/databinding/FragmentCommuBinding.java
+++ front-end/umgrade/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/umgrade/databinding/FragmentCommuBinding.java
@@ -4,20 +4,33 @@ package com.example.umgrade.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.umgrade.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentCommuBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private FragmentCommuBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final Button button;
+
+  @NonNull
+  public final TextView textView2;
+
+  private FragmentCommuBinding(@NonNull ConstraintLayout rootView, @NonNull Button button,
+      @NonNull TextView textView2) {
     this.rootView = rootView;
+    this.button = button;
+    this.textView2 = textView2;
   }
 
   @Override
@@ -43,10 +56,25 @@ public final class FragmentCommuBinding implements ViewBinding {
 
   @NonNull
   public static FragmentCommuBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.button;
+      Button button = ViewBindings.findChildViewById(rootView, id);
+      if (button == null) {
+        break missingId;
+      }
 
-    return new FragmentCommuBinding((ConstraintLayout) rootView);
+      id = R.id.textView2;
+      TextView textView2 = ViewBindings.findChildViewById(rootView, id);
+      if (textView2 == null) {
+        break missingId;
+      }
+
+      return new FragmentCommuBinding((ConstraintLayout) rootView, button, textView2);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
--- front-end/umgrade/app/src/main/java/com/example/umgrade/CommuFragment.java
+++ front-end/umgrade/app/src/main/java/com/example/umgrade/CommuFragment.java
@@ -8,13 +8,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firestore.v1.Write;
 
 
 public class CommuFragment extends Fragment {
+    Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_commu, container, false);
+
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), WriteActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
