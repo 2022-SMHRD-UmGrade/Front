b0714fafda21076420e6d19f78f8e7b6f9ad89e6
yeeun-b <yeeun.park.417@gmail.com>
2022-05-13 15:32:16 +0900

    이벤트 리스트뷰 생성 -예은-

--- front-end/umgrade/app/src/main/java/com/example/umgrade/EventFragment.java
+++ front-end/umgrade/app/src/main/java/com/example/umgrade/EventFragment.java
@@ -7,13 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class EventFragment extends Fragment {
+    ListView lvEvent; // 이벤트 리스트뷰
+
+    TextView eventTitle, eventStart, eventEnd, eventCnt;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_event, container, false);
+
+        eventTitle = view.findViewById(R.id.eventTitle); // 글제목
+        eventStart = view.findViewById(R.id.eventStart); // 시작 날짜
+        eventEnd = view.findViewById(R.id.eventEnd); // 종료날짜
+        eventCnt = view.findViewById(R.id.eventCnt); // 글번호
 
         return view;
     }
