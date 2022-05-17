bd984cd909321301e675d0b8fec1cd48da58de92
yeeun-b <yeeun.park.417@gmail.com>
2022-05-17 14:55:24 +0900

    더보기 프래그먼트 연결 -예은-

--- front-end/umgrade/app/src/main/java/com/example/umgrade/main/MainActivity.java
+++ front-end/umgrade/app/src/main/java/com/example/umgrade/main/MainActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.umgrade.MoreFragment;
 import com.example.umgrade.R;
 import com.example.umgrade.communityFrag.CommuFragment;
 import com.example.umgrade.mainFrag.MainFragment;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     MainFragment frag_main;
     CommuFragment frag_commu;
     MypageFragment frag_mypage;
+    MoreFragment frag_more;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         frag_main = new MainFragment();
         frag_commu = new CommuFragment();
         frag_mypage = new MypageFragment();
+        frag_more = new MoreFragment();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, frag_main).commit();
 
@@ -57,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, frag_commu).commit();
                 } else if (itemId == R.id.fragMyPage){
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, frag_mypage).commit();
+                } else if (itemId == R.id.fragMore){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, frag_more).commit();
                 }
 
                 return true;
